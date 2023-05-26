bf7473e4ccb16f8f6f0ba70f0f6a9f2933a0e845
loicberland <berland.loic@yahoo.fr>
2023-05-26 16:27:18 +0200

    ajout func option

--- main.go
+++ main.go
@@ -10,22 +10,20 @@ import (
 )
 
 func main() {
+	optionText := `Options :
+	- Recherche les dossiers et fichiers => 1 (default) 
+	- Recherche seulement les dossiers => 2 
+	- Recherche seulement les fichiers => 3 
+	- Recherche les dossiers, les fichiers, les sous-dossiers et les sous-fichiers => 4 :`
+
 	dirPath := askValue("Path du dossier à scanner (si vide on prend le path de l'exe) : ")
 	inputSearch := askValue("Rechercher : ")
 	inputReplace := askValue("Remplacer par : ")
-	// reader := bufio.NewReader(os.Stdin)
-	// scanner := bufio.NewScanner(reader)
-	// // Demander chemin
-	// //	- Si pas de chemin rechercher dans dossier actuel
-	// fmt.Print("Path du dossier à scanner (si vide on prend le path de l'exe) : ")
-	// scanner.Scan()
-	// dirPath := scanner.Text()
-	// fmt.Print("Rechercher : ")
-	// scanner.Scan()
-	// inputSearch := scanner.Text()
-	// fmt.Print("Remplacer par : ")
-	// scanner.Scan()
-	// inputReplace := scanner.Text()
+	optionChoice := askValue(optionText)
+	for checkOptionChoice(optionChoice) == false {
+		optionChoice = askValue(optionText)
+	}
+	fmt.Println(optionChoice)
 	if dirPath == "" {
 		// _, dir, _, _ := runtime.Caller(0) //Mode débug
 		dir, err := os.Executable() //Mode Prod
@@ -42,7 +40,25 @@ func main() {
 	fmt.Scanln()
 
 }
-
+func checkOptionChoice(optionChoice string) bool {
+	switch optionChoice {
+	case "1", "":
+		fmt.Println("Option choisie : recherche les dossiers et fichiers.")
+		return true
+	case "2":
+		fmt.Println("Option choisie : recherche seulement les dossiers.")
+		return true
+	case "3":
+		fmt.Println("Option choisie : recherche seulement les fichiers.")
+		return true
+	case "4":
+		fmt.Println("Option choisie : recherche les dossiers, les fichiers, les sous-dossiers et les sous-fichiers.")
+		return true
+	default:
+		fmt.Println("Entrée incorrecte !!!")
+		return false
+	}
+}
 func askValue(question string) string {
 	reader := bufio.NewReader(os.Stdin)
 	scanner := bufio.NewScanner(reader)
@@ -53,6 +69,7 @@ func askValue(question string) string {
 func findAndReplaceString(dirPath, searchString, replaceString string) {
 	files, errFiles := os.ReadDir(dirPath)
 	countRename := 0
+	//On récupère le nom de l'exe
 	dir, _ := os.Executable()
 	exeName := filepath.Base(dir)
 	//Vérification path correct
@@ -60,7 +77,7 @@ func findAndReplaceString(dirPath, searchString, replaceString string) {
 		log.Fatal(errFiles)
 	}
 	for _, file := range files {
-		if file.Name() != exeName {
+		if file.Name() != exeName { //Sécuritée pour le pas renommer l'exe
 			if strings.Contains(file.Name(), searchString) {
 				newName := strings.ReplaceAll(file.Name(), searchString, replaceString)
 				oldPath := filepath.Join(dirPath, file.Name())
